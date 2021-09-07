00e2e2a396b7c43cb99b187515567beb44d14c8b
gaw1ik <64555628+gaw1ik@users.noreply.github.com>
2021-09-07 14:01:27 -0500

    1.17c
    updated the icons in the layer controls

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@
 // handles the window.onload tasks and also sets up event handlers
 
 
-VERSION = "1.17b"
+VERSION = "1.17c"
 console.log("Welcome to genartlayers (VERSION ",VERSION," )");
 
 // GLOBAL VARIABLES
