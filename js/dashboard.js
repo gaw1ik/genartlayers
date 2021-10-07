a56020f589ed69e67c70ebebcdee9f170c560463
gaw1ik <64555628+gaw1ik@users.noreply.github.com>
2021-10-07 13:04:29 -0500

    1.21d
    oops forgot to update the welcome message

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@
 // handles the window.onload tasks and also sets up event handlers
 
 
-VERSION = "1.21b"
+VERSION = "1.21d"
 console.log("Welcome to genartlayers (VERSION ",VERSION," )");
 
 // GLOBAL VARIABLES
